e1753c70aa8ebec61f98fda521c81b75b93627e3
JuliaOJustino <125585504+JuliaOJustino@users.noreply.github.com>
2023-03-06 11:13:03 -0300

    primeira versão
    Calculadora começando

---
+++ primeiraVersao/script.js
@@ -0,0 +1,7 @@
+function func_form() {
+    console.log("A função está sendo chamada! sucesso");
+    if (inp_perimetro.value < 0 || inp_duracao.value < 0){
+       alert("Número inválido, preencha novamente");
+    }
+    
+}
